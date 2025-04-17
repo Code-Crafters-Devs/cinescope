28b239ef85ac6bddd813a81cb880e57429a75f2a
Khulekani Mtshali <mkhulekani.469@gmail.com>
2025-04-17 14:35:04 +0200

    URL Update

--- cinescope-frontend/src/components/auth/SignupForm.jsx
+++ cinescope-frontend/src/components/auth/SignupForm.jsx
@@ -49,7 +49,7 @@ function SignupForm() {
       try {
         const { confirmPassword, ...dataToSend } = formData;
         
-        const response = await fetch('http://localhost:5000/api/auth/register', {
+        const response = await fetch('https://cinescope-backend-jany.onrender.com/api/auth/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
