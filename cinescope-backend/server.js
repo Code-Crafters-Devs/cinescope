1c04e103cd03c349d3d0319db6d4f1a8a1932224
Khulekani Mtshali <mkhulekani.469@gmail.com>
2025-04-17 14:25:41 +0200

    Fixing the server

--- cinescope-backend/server.js
+++ cinescope-backend/server.js
@@ -12,7 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://cinescope-frontend-y9ee.onrender.com'
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
